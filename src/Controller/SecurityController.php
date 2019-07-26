2e7965601c040de658ab10fe069fc8bbc9677f1c
babacar44 <babacamara91@gmail.com>
2019-07-26 20:25:19 +0000

    table user

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+/**
+ * @Route("/api")
+ */
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/register", name="register", methods={"POST"})
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, EntityManagerInterface $entityManager, SerializerInterface $serializer, ValidatorInterface $validator)
+    {
+        $values = json_decode($request->getContent());
+        if(isset($values->email,$values->password)) {
+            $user = new User();
+            $user->setEmail($values->email);
+            $user->setPassword($passwordEncoder->encodePassword($user, $values->password));
+            $user->setRoles($values->roles);
+            $user->setNomcomplet($values->nomcomplet);
+            $user->setPropriete($values->propriete);
+            $user->setAdresse($values->adresse);
+            $user->setTelephone($values->telephone);
+            $user->setStatut($values->statut);
+            $errors = $validator->validate($user);
+            if(count($errors)) {
+                $errors = $serializer->serialize($errors, 'json');
+                return new Response($errors, 500, [
+                    'Content-Type' => 'application/json'
+                ]);
+            }
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $data = [
+                'status' => 201,
+                'message' => 'L\'utilisateur a été créé'
+            ];
+
+            return new JsonResponse($data, 201);
+        }
+        $data = [
+            'status' => 500,
+            'message' => 'Vous devez renseigner les clés email et password'
+        ];
+        return new JsonResponse($data, 500);
+    }
+
+    /**
+     * @Route("/login", name="login", methods={"POST"})
+     */
+    public function login(Request $request)
+    {
+        $user = $this->getUser();
+        return $this->json([
+            'email' => $user->getUsername(),
+            'roles' => $user->getRoles()
+        ]);
+    }
+}
