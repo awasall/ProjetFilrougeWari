ede06b70810a127226801e7b25eaabfa18825651
awasall <sallawa622@gmail.com>
2019-07-30 10:38:50 +0000

    dddd

--- tests/PartenaireControllerTest.php
+++ tests/PartenaireControllerTest.php
@@ -8,66 +8,83 @@ class PartenaireControllerTest extends WebTestCase
 {
     public function testIndex()
     {
-        $client = static::createClient();
+        $client = static::createClient([],[
+            'PHP_AUTH_USER'=>"diama@gmail.com",
+            'PHP_AUTH_PW'=>"passer"
+
+        ]);
         $crawler = $client->request('GET', '/api/listepartenaire');
 
         $rep=$client->getResponse();
-        $this->assertSame(401,$client->getResponse()->getStatusCode());
+        $this->assertSame(200,$client->getResponse()->getStatusCode());
         //$this->assertJsonStringEqualsJsonString($jsonstring,$rep->getContent());
     }
-    public function testAjoutOk()
+    public function testAjoutpartenaire()
     {
-        $client = static::createClient();
+        $client = static::createClient([],[
+            'PHP_AUTH_USER'=>"diama@gmail.com",
+            'PHP_AUTH_PW'=>"passer"
+
+        ]);
         $crawler = $client->request('POST', '/api/partenaire',[],[],
         ['CONTENT_TYPE'=>"application/json"],
         '{
-            "ninea":"0001C",
-            "raisonsociale":"multiservice",
+            "ninea":"0001L",
+            "raisonsociale":"Ndoyeservice",
             "adresse":"Dakar",
-            "telephone":"776778275",
-            "numerocompte":"654987231",
+            "telephone":"787878795",
+            "numerocompte":"473659",
             "solde":"0",
-            "email":"mohamed@gmail.com"
+            "email":"ndoye@gmail.com"
             }');
         $rep=$client->getResponse();
         var_dump($rep);
-        $this->assertSame(401,$client->getResponse()->getStatusCode());
+        $this->assertSame(201,$client->getResponse()->getStatusCode());
     }
-
+    
 
     public function testAjoutDepot()
     {
-        $client = static::createClient();
+        $client = static::createClient([],[
+            'PHP_AUTH_USER'=>"ousseynou@gmail.com",
+            'PHP_AUTH_PW'=>"passer"
+
+        ]);
         $crawler = $client->request('POST', '/api/depot',[],[],
         ['CONTENT_TYPE'=>"application/json"],
         '{
-            "montant":700000,
-            "partenaire":27
+            "montant":800000,
+            "partenaire":46
             }');
         $rep=$client->getResponse();
         var_dump($rep);
-        $this->assertSame(401,$client->getResponse()->getStatusCode());
+        $this->assertSame(201,$client->getResponse()->getStatusCode());
     }
 
 
     public function testAjoutUtilisateur()
     {
-        $client = static::createClient();
+        $client = static::createClient([],[
+            'PHP_AUTH_USER'=>"diama@gmail.com",
+            'PHP_AUTH_PW'=>"passer"
+
+        ]);
         $crawler = $client->request('POST', '/api/register',[],[],
         ['CONTENT_TYPE'=>"application/json"],
         '{
-            "email":"ousseynou@gmail.com",
-            "roles":["ROLE_caissier"],
+            "email":"jean@gmail.com",
             "password":"passer",
-            "nomcomplet":"ousseynou sall",
+            "password_confirmation":"passer",
+            "roles":["ROLE_caissier"],
+            "nomcomplet":"jean sall",
             "propriete":"Wari",
             "adresse":"parcelles",
-            "telephone":"769876542",
+            "telephone":"7676895169",
             "statut":"actif"
             
             }');
         $rep=$client->getResponse();
         var_dump($rep);
-        $this->assertSame(401,$client->getResponse()->getStatusCode());
+        $this->assertSame(201,$client->getResponse()->getStatusCode());
     }
 }
